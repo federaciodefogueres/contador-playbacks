9b152412d2000d8b98120852fbc2b3d84acaadc8
Fran García <fgarciadevelop@gmail.com>
2023-11-07 23:51:36 +0100

    feat: add alerts to app

--- src/app/components/confirm-registration/confirm-registration.component.ts
+++ src/app/components/confirm-registration/confirm-registration.component.ts
@@ -1,7 +1,7 @@
 import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { EmailItem, SettingsService } from 'src/api';
+import { EmailItem, InlineResponse200, SettingsService } from 'src/api';
 import { TimerService, TimerStatus } from 'src/app/services/timer.service';
 
 @Component({
@@ -10,6 +10,9 @@ import { TimerService, TimerStatus } from 'src/app/services/timer.service';
   styleUrls: ['./confirm-registration.component.scss']
 })
 export class ConfirmRegistrationComponent {
+
+  emailStatus = '';
+  alertMessage = '';
 
   calculatedSize = {
     width: 0,
@@ -50,7 +53,7 @@ export class ConfirmRegistrationComponent {
 
   loadForm() {
     this.registryForm = this.fb.group({
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       entryTimer: new FormControl(this.entryTimer.value, Validators.required),
       exitTimer: new FormControl(this.exitTimer.value, Validators.required),
       sign: new FormControl('', Validators.required)
@@ -59,30 +62,43 @@ export class ConfirmRegistrationComponent {
 
   sendEmail() {
     this.saveCanvas();
-    let body: EmailItem = {
-      subject: 'Envío de resultados FFSJ',
-      content: `
-        <h1>XXXIII Certamen Artístico Fogueres de Sant Joan</h1>
-        <h3>Modalidad Única - Sesión 11 Noviembre 2023 11:00h</h3>
-        <p>¡Hola! Este es un coreo automático generado por nuestro asistente virtual. El tiempo registrado por tu asociación han sido los siguientes:</p>
-        <div style="display: flex;">
-            <h3>Tiempo de entrada ${this.registryForm.controls['entryTimer'].value}</h3>
-        </div>
-        <div style="display: flex;">
-            <h3>Tiempo de salida ${this.registryForm.controls['exitTimer'].value}</h3>
-        </div>
-
-        <h3>Un responsable de vuestra asociación ha firmado en la aplicación web constatando estar de acuerdo con los tiempos registrados. Aquí tenéis, como archivo adjunto, la firma.</h3>
-
-        <h3>Nos vemos en la próxima! :)</h3>
-      `,
-      destine: this.registryForm.controls['email'].value,
-      dataSesion: 'asd',
-      sign: this.registryForm.controls['sign'].value
+    if (this.registryForm.valid) {
+      this.emailStatus = 'sending';
+      this.alertMessage = `Enviando email a ${this.registryForm.controls['email'].value}`
+      let body: EmailItem = {
+        subject: 'Envío de resultados FFSJ',
+        content: `
+          <h1>XXXIII Certamen Artístico Fogueres de Sant Joan</h1>
+          <h3>Modalidad Única - Sesión 11 Noviembre 2023 11:00h</h3>
+          <p>¡Hola! Este es un coreo automático generado por nuestro asistente virtual. El tiempo registrado por tu asociación han sido los siguientes:</p>
+          <div style="display: flex;">
+              <h3>Tiempo de entrada ${this.registryForm.controls['entryTimer'].value}</h3>
+          </div>
+          <div style="display: flex;">
+              <h3>Tiempo de salida ${this.registryForm.controls['exitTimer'].value}</h3>
+          </div>
+  
+          <h3>Un responsable de vuestra asociación ha firmado en la aplicación web constatando estar de acuerdo con los tiempos registrados. Aquí tenéis, como archivo adjunto, la firma.</h3>
+  
+          <h3>Nos vemos en la próxima! :)</h3>
+        `,
+        destine: this.registryForm.controls['email'].value,
+        dataSesion: 'asd',
+        sign: this.registryForm.controls['sign'].value
+      }
+      this.settingsService.sendEmail(body).subscribe((res: InlineResponse200) => {
+        if (res.status?.status === '200') {
+          this.emailStatus = 'sentOK';
+          this.alertMessage = '¡Email enviado!';
+        } else {
+          this.emailStatus = 'sentKO';
+          this.alertMessage = 'No se pudo enviar el email.';
+        }
+      })
+    } else {
+      this.emailStatus = 'sentKO';
+      this.alertMessage = 'Hay datos que no han sido rellenados. Rellénalos!';
     }
-    this.settingsService.sendEmail(body).subscribe(res => {
-      console.log(res)
-    })
   }
 
   saveCanvas() {
