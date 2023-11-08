bd15a14e1b2945a2ec7db0479752a6ffd64e4f87
Fran García <fgarciadevelop@gmail.com>
2023-11-08 19:24:18 +0100

    refactor: add new swagger changes

--- src/api/model/models.ts
+++ src/api/model/models.ts
@@ -1,3 +1,7 @@
+export * from './asociacion';
+export * from './asociacionesResponse';
 export * from './emailItem';
 export * from './inlineResponse200';
 export * from './response200';
+export * from './session';
+export * from './sessionsResponse';
