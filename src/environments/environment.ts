acbf07d50da1dc90f69a532236ab34accd3a1094
Fran García <fgarciadevelop@gmail.com>
2023-11-13 16:50:08 +0100

    feat: firebase config ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
     production: false,
-    API_BASE_PATH: 'http://localhost:3000/federaciodefogueres/contador-playbacks-api/1.0.0'
+    API_BASE_PATH: 'http://localhost:3000/federaciodefogueres/contador-playbacks-api/1.0.0',
+    firebase: {
+      apiKey: "AIzaSyDHqm8LmUHebUWewJj-joJiyZEjGiItSB8",
+      authDomain: "contador-playbacks.firebaseapp.com",
+      projectId: "contador-playbacks",
+      storageBucket: "contador-playbacks.appspot.com",
+      messagingSenderId: "27950022924",
+      appId: "1:27950022924:web:82e2c0c5a769466a50c929",
+      measurementId: "G-6QDHJMLHDV"
+    }
   };
   
   /*
